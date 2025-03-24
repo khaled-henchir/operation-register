4a99079100177257f2094f687c18a6b6f48df278
khaled-henchir <khaledhenchir0@gmail.com>
2025-03-24 15:08:38 +0100

    cleaned unused package.json packages

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 import { createServer } from './infrastructure/http/app';
+import 'dotenv/config'; 
 
 const app = createServer();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Swagger API documentation : HOST:${PORT}/api-docs`);
+  console.log(`Swagger API documentation: http://localhost:${PORT}/api-docs`);
 });
